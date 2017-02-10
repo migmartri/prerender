12f6410b234054ea015724d11725453e6d1650ea
Miguel Martinez <miguel@bitnami.com>
2017-02-10 13:51:38 -0800

    Cache env variables

--- server.js
+++ server.js
@@ -14,7 +14,18 @@ server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
+
+// Cache
+if (process.env.IN_MEMORY_CACHE) {
+  server.use(prerender.inMemoryHtmlCache());
+}
+
+if (
+  process.env.AWS_ACCESS_KEY_ID &&
+  process.env.AWS_SECRET_ACCESS_KEY &&
+  process.env.S3_BUCKET_NAME
+) {
+  server.use(prerender.s3HtmlCache());
+}
 
 server.start();
